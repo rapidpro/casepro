60f86d127a701f451f61051dc355ebb56df9f82a
Rowan Seymour <rowanseymour@gmail.com>
2015-11-19 16:59:07 +0200

    Updated configuration files for new upartners.org domain

--- fabconfig.py
+++ fabconfig.py
@@ -1,20 +1,29 @@
-config = dict(    
-    port='8030',
-    app_dir='casepro',
-    friendly_name='CasePro',
-    repository='ssh://git@github.com/rapidpro/casepro.git',
-    domain='partners.ureport.in',
-    name='casepro',
-    repo='casepro',
-    user='casepro',
-    env='env',
-    settings='settings.py.dev',
-    dbms='psql',
-    db='casepro',
-    custom_domains='*.partners.ureport.in partners.ureport.in upartners.staging.nyaruka.com *.upartners.staging.nyaruka.com',
-    prod_host='partners.ureport.in',
-    sqldump=False,
-    celery=True,
-    processes=('celery',),
-    compress=True,
-)
+config = {
+    'port': '8030',
+    'app_dir': 'casepro',
+    'friendly_name': 'CasePro',
+    'repository': 'ssh://git@github.com/rapidpro/casepro.git',
+    'domain': 'upartners.org',
+    'name': 'casepro',
+    'repo': 'casepro',
+    'user': 'casepro',
+    'env': 'env',
+    'settings': 'settings.py.dev',
+    'dbms': 'psql',
+    'db': 'casepro',
+    'custom_domains': '*.upartners.org upartners.org *.partners.ureport.in partners.ureport.in upartners.staging.nyaruka.com *.upartners.staging.nyaruka.com',
+    'prod_host': 'case1',
+    'sqldump': False,
+    'celery': True,
+    'processes': ('celery',),
+    'compress': True,
+    'elb': {
+        'name': 'UPartners',
+        'region': 'eu-west-1',
+        'primary': 'partner1',
+        'instances': [
+            {'name': 'partner1', 'host': 'partner1.upartners.org', 'id': 'i-5ccaec1f'},
+            {'name': 'partner2', 'host': 'partner1.upartners.org', 'id': 'i-e89fd8aa'}
+        ]
+    }
+}
