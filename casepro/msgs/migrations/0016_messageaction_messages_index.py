cfe8e810180402594fb5cd392f6074fda22c7f2c
Rowan Seymour <rowanseymour@gmail.com>
2016-02-27 14:12:51 +0200

    Added GIN index on MessageAction.messages

---
+++ casepro/msgs/migrations/0016_messageaction_messages_index.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('msgs', '0015_message_is_active'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            'CREATE INDEX msgs_messageaction_messages_idx ON msgs_messageaction USING GIN ("messages");'
+        ),
+    ]
