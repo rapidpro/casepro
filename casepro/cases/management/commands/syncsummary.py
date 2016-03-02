2892b9189fb3909c4d549b388ae5d05960ca09a2
Rowan Seymour <rowanseymour@gmail.com>
2016-03-02 13:30:24 +0200

    Updated syncsummary command to include counts of inactive items

--- casepro/cases/management/commands/syncsummary.py
+++ casepro/cases/management/commands/syncsummary.py
@@ -24,42 +24,79 @@ class Command(BaseCommand):
         if org_ids:
             orgs = orgs.filter(pk__in=org_ids)
 
+        self.do_contacts(orgs)
+        self.do_messages(orgs)
+
+    def do_contacts(self, orgs):
+        self.stdout.write("\nSummarizing contacts for %d orgs...\n\n" % len(orgs))
         header = [
             cell("ID", 4),
             cell("Name", 16),
-            cell("Contacts", 12),
-            cell("Stubs", 8),
-            cell("Stuck", 8),
-            cell("Messages", 12),
-            cell("Unhandled", 10),
-            cell("Stuck", 8),
+            cell("Total", 12),
+            cell("Inactive", 10),
+            cell("Stubs", 10),
+            cell("Stuck", 10),
         ]
 
         self.stdout.write("".join(header))
-        self.stdout.write("===========================================================================")
+        self.stdout.write("==================================================================")
 
         an_hour_ago = now() - timedelta(hours=1)
 
         for org in orgs:
-            contacts = org.contacts.filter(is_active=True)
-            total_contacts = contacts.count()
-            stub_contacts = contacts.filter(is_stub=True).count()
-            stuck_stub_contacts = contacts.filter(is_stub=True, created_on__lt=an_hour_ago).count()
+            active = org.contacts.filter(is_active=True)
+            inactive = org.contacts.filter(is_active=False)
 
-            messages = org.incoming_messages.filter(is_active=True)
-            total_messages = messages.count()
-            unhandled_messages = messages.filter(is_handled=False).count()
-            stuck_unhandled_messages = messages.filter(is_handled=False, created_on__lt=an_hour_ago).count()
+            num_active = active.count()
+            num_inactive = inactive.count()
+            num_total = num_active + num_inactive
+            num_stubs = active.filter(is_stub=True).count()
+            num_stuck = active.filter(is_stub=True, created_on__lt=an_hour_ago).count()
 
             cells = [
                 cell(org.id, 4),
                 cell(org.name, 16),
-                cell(total_contacts, 12),
-                cell(stub_contacts, 8),
-                cell(stuck_stub_contacts, 8),
-                cell(total_messages, 12),
-                cell(unhandled_messages, 10),
-                cell(stuck_unhandled_messages, 8),
+                cell(num_total, 12),
+                cell(num_inactive, 10),
+                cell(num_stubs, 10),
+                cell(num_stuck, 10),
+            ]
+
+            self.stdout.write("".join(cells))
+
+    def do_messages(self, orgs):
+        self.stdout.write("\nSummarizing messages for %d orgs...\n\n" % len(orgs))
+        header = [
+            cell("ID", 4),
+            cell("Name", 16),
+            cell("Total", 12),
+            cell("Inactive", 10),
+            cell("Unhandled", 10),
+            cell("Stuck", 10),
+        ]
+
+        self.stdout.write("".join(header))
+        self.stdout.write("==================================================================")
+
+        an_hour_ago = now() - timedelta(hours=1)
+
+        for org in orgs:
+            active = org.incoming_messages.filter(is_active=True)
+            inactive = org.incoming_messages.filter(is_active=False)
+
+            num_active = active.count()
+            num_inactive = inactive.count()
+            num_total = num_active + num_inactive
+            num_unhandled = org.incoming_messages.filter(is_handled=False).count()
+            num_stuck = org.incoming_messages.filter(is_handled=False, created_on__lt=an_hour_ago).count()
+
+            cells = [
+                cell(org.id, 4),
+                cell(org.name, 16),
+                cell(num_total, 12),
+                cell(num_inactive, 10),
+                cell(num_unhandled, 10),
+                cell(num_stuck, 10),
             ]
 
             self.stdout.write("".join(cells))
