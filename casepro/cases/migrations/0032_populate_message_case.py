45b7c933c22b6a2650cb186fea3335791511735f
Rowan Seymour <rowanseymour@gmail.com>
2016-03-05 13:53:30 +0200

    Added migration to associate messages added before refactor with cases

---
+++ casepro/cases/migrations/0032_populate_message_case.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def populate_message_case(apps, schema_editor):
+    Case = apps.get_model('cases', 'Case')
+    Message = apps.get_model('msgs', 'Message')
+
+    cases = list(Case.objects.order_by('org', 'opened_on'))
+    num_updated = 0
+
+    for case in cases:
+        missing_messages = Message.objects.filter(org=case.org, contact=case.contact, case=None)
+        missing_messages = missing_messages.filter(created_on__gte=case.opened_on)
+
+        if case.closed_on:
+            missing_messages = missing_messages.filter(created_on__lte=case.closed_on)
+
+        num_updated += missing_messages.update(case=case)
+
+    if cases:
+        print "Attached %d missing messages to %d cases" % (num_updated, len(cases))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cases', '0031_remove_caseevent'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_message_case)
+    ]
