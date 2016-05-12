7c520b046b5a46321b82a024a404422a053db291
Rowan Seymour <rowanseymour@gmail.com>
2016-05-12 09:10:03 +0200

    Added migration to unset dynamic groups as suspend_from groups

---
+++ casepro/contacts/migrations/0020_unset_suspend_from_dynamic.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def unset_dynamic_suspend_groups(apps, schema_editor):
+    Group = apps.get_model('contacts', 'Group')
+    num_updated = Group.objects.filter(is_dynamic=True, suspend_from=True).update(suspend_from=False)
+    if num_updated:
+        print("Updated %d dynamic groups to not be suspend_from groups")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contacts', '0019_group_is_dynamic'),
+    ]
+
+    operations = [
+        migrations.RunPython(unset_dynamic_suspend_groups)
+    ]
