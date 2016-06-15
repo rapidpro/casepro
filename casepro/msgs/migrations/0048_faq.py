9f842e249bf307134bc9b93abc0c98bba6d33f35
gsvr <george@westerncapelabs.com>
2016-06-15 18:27:08 +0200

    first faq model attempts

---
+++ casepro/msgs/migrations/0048_faq.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orgs', '0016_taskstate_is_disabled'),
+        ('msgs', '0047_outgoing_urn'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FAQ',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question', models.CharField(max_length=140)),
+                ('answer', models.CharField(max_length=140)),
+                ('labels', models.ManyToManyField(help_text='Labels assigned to this FAQ', related_name='faqs', to='msgs.Label')),
+                ('org', models.ForeignKey(related_name='faqs', verbose_name='Organization', to='orgs.Org')),
+            ],
+        ),
+    ]
