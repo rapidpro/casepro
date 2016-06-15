ebe47755e66bec3d9df64f4ac9ed51b878de787a
gsvr <george@westerncapelabs.com>
2016-06-15 16:36:49 +0200

    add faq to urls

--- casepro/msgs/urls.py
+++ casepro/msgs/urls.py
@@ -1,9 +1,10 @@
 from __future__ import unicode_literals
 
-from .views import LabelCRUDL, MessageCRUDL, MessageExportCRUDL, OutgoingCRUDL, ReplyExportCRUDL
+from .views import LabelCRUDL, FaqCRUDL, MessageCRUDL, MessageExportCRUDL, OutgoingCRUDL, ReplyExportCRUDL
 
 urlpatterns = LabelCRUDL().as_urlpatterns()
 urlpatterns += MessageCRUDL().as_urlpatterns()
+urlpatterns += FaqCRUDL().as_urlpatterns()
 urlpatterns += MessageExportCRUDL().as_urlpatterns()
 urlpatterns += OutgoingCRUDL().as_urlpatterns()
 urlpatterns += ReplyExportCRUDL().as_urlpatterns()
