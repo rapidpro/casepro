ff9f6acb0b60220076ce30809d051876cdc2b910
gsvr <george@westerncapelabs.com>
2016-06-23 13:51:58 +0200

    add faq search permission

--- casepro/settings_common.py
+++ casepro/settings_common.py
@@ -307,7 +307,7 @@ PERMISSIONS = {
 
     'msgs.label': ('create', 'update', 'read', 'delete', 'list'),
 
-    'msgs.faq': ('create', 'read', 'update', 'delete', 'list'),
+    'msgs.faq': ('create', 'read', 'update', 'delete', 'list', 'search'),
 
     'msgs.language': ('create', 'read', 'update', 'delete', 'list'),
 
