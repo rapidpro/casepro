338feb918b2d11f1fe44fa0f786519799a82736f
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-07-01 13:29:37 +0200

    Set IdentityStoreContact attributes from dict given

--- casepro/backend/junebug.py
+++ casepro/backend/junebug.py
@@ -66,16 +66,10 @@ class IdentityStoreContact(object):
     Holds identity data for syncing
     """
     def __init__(self, json_data):
-        self.id = json_data.get('id')
-        self.version = json_data.get('version')
+        for k, v in json_data.items():
+            setattr(self, k, v)
         self.name = json_data.get('details').get('name')
         self.addresses = json_data.get('details').get('addresses')
-        self.communicate_through = json_data.get('communicate_through')
-        self.operator = json_data.get('operator')
-        self.created_at = json_data.get('created_at')
-        self.created_by = json_data.get('created_by')
-        self.updated_at = json_data.get('updated_at')
-        self.updated_by = json_data.get('updated_by')
 
 
 class IdentityStoreContactSyncer(BaseSyncer):
