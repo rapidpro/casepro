1607dcb58fafe4c04dd848c76f6aa668fc558e0f
Rudi Giesler <rudi@praekelt.com>
2016-07-13 09:14:16 +0200

    Add base Pod objects and configs.

---
+++ casepro/pods/__init__.py
@@ -0,0 +1,3 @@
+from .base import Pod, PodConfig, PodPlugin
+
+__all__ = ['Pod', 'PodConfig', 'PodPlugin']
---
+++ casepro/pods/base.py
@@ -0,0 +1,31 @@
+from confmodel import fields, Config as ConfmodelConfig
+from django.apps import AppConfig
+
+
+class PodPlugin(AppConfig):
+    pass
+
+
+class PodConfig(ConfmodelConfig):
+    '''
+    This is the config that all pods should use as the base for their own
+    config.
+    '''
+    index = fields.ConfigInt(
+        "A unique identifier for the specific instance of this pod."
+        "Automatically determined and set in the pod registry.",
+        required=True)
+
+
+class Pod(object):
+    '''
+    The base class for all pod plugins.
+    '''
+    config_cls = PodConfig
+    url_patterns = []
+    name = 'Default pod name'
+    controller = None
+    directive = None
+
+    def __init__(self, config):
+        self.config = self.config_cls(config)
