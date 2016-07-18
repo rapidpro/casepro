0f9b5f3b2742e19c1727d3899fce8406b93b969a
gsvr <george@westerncapelabs.com>
2016-07-18 09:54:34 +0200

    add description to partner

--- casepro/test.py
+++ casepro/test.py
@@ -48,9 +48,10 @@ class BaseCasesTest(DashTest):
         self.code = self.create_label(self.nyaruka, "L-101", "Code", 'Messages about code', ["java", "python", "go"])
 
         # some partners
-        self.moh = self.create_partner(self.unicef, "MOH", "Africa/Kampala", [self.aids, self.pregnancy])
-        self.who = self.create_partner(self.unicef, "WHO", "Africa/Kampala", [self.aids])
-        self.klab = self.create_partner(self.nyaruka, "kLab", "Africa/Kigali", [self.code])
+        self.moh = self.create_partner(self.unicef, "MOH", "Ministry of Health", "Africa/Kampala",
+                                       [self.aids, self.pregnancy])
+        self.who = self.create_partner(self.unicef, "WHO", "World Health Organisation", "Africa/Kampala", [self.aids])
+        self.klab = self.create_partner(self.nyaruka, "kLab", "Kigali Lab", "Africa/Kigali", [self.code])
 
         # some users in those partners
         self.user1 = self.create_user(self.unicef, self.moh, ROLE_MANAGER, "Evan", "evan@unicef.org")
@@ -71,8 +72,8 @@ class BaseCasesTest(DashTest):
         self.state = self.create_field(self.unicef, 'state', "State", value_type='S', is_visible=False)
         self.motorbike = self.create_field(self.nyaruka, 'motorbike', "Moto", value_type='T')
 
-    def create_partner(self, org, name, timezone, labels=(), restricted=True):
-        return Partner.create(org, name, timezone, restricted, labels, None)
+    def create_partner(self, org, name, description, timezone, labels=(), restricted=True):
+        return Partner.create(org, name, description, timezone, restricted, labels, None)
 
     def create_admin(self, org, name, email):
         return Profile.create_org_user(org, name, email, email)
