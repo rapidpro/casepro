b7e7792109df276e9dc180f858f988ac9e3fc33b
justinvdm <justinvdm.0.1.0@gmail.com>
2016-08-05 17:57:34 +0200

    Restrict access for case views

--- casepro/pods/views.py
+++ casepro/pods/views.py
@@ -3,9 +3,8 @@ from __future__ import unicode_literals
 import json
 from django.http import JsonResponse
 
-from casepro.cases.models import Case, CaseAction
+from casepro.cases.models import Case, CaseAction, AccessLevel
 from casepro.pods import registry
-
 
 ACTION_NOTE_CONTENT = "%(username)s %(message)s"
 
@@ -14,6 +13,19 @@ def read_pod_data(request, index):
     """Delegates to the `read_data` function of the correct pod."""
     if request.method != 'GET':
         return JsonResponse({'reason': 'Method not allowed'}, status=405)
+
+    case_id = request.GET.get('case_id')
+    if case_id is None:
+        return JsonResponse(status=400, data={
+            'reason': 'Request needs "case_id" query parameter'
+        })
+
+    case = get_case(case_id)
+    if case is None:
+        return case_not_found_response(case_id)
+
+    if not has_case_access(request.user, case, AccessLevel.read):
+        return authorization_failure_response()
 
     try:
         pod = registry.pods[int(index)]
@@ -46,10 +58,16 @@ def perform_pod_action(request, index):
         return JsonResponse(
             {'reason': 'Request object needs to have a "case_id" field'}, status=400)
 
+    case = get_case(case_id)
+    if case is None:
+        return case_not_found_response(case_id)
+
+    if not has_case_access(request.user, case, AccessLevel.update):
+        return authorization_failure_response()
+
     action_data = data.get('action', {})
     success, payload = pod.perform_action(action_data.get('type'), action_data.get('payload', {}))
     if success is True:
-        case = Case.objects.get(id=case_id)
         note = ACTION_NOTE_CONTENT % {
             'username': request.user.username,
             'message': payload.get('message'),
@@ -57,3 +75,25 @@ def perform_pod_action(request, index):
         CaseAction.create(case, request.user, CaseAction.ADD_NOTE, note=note)
 
     return JsonResponse({'success': success, 'payload': payload})
+
+
+def has_case_access(user, case, level):
+    return case.access_level(user) >= level
+
+
+def get_case(case_id):
+    return Case.objects.filter(id=case_id).first()
+
+
+def case_not_found_response(id):
+    return JsonResponse(status=404, data={
+        'reason': "Case with id %s not found" % id
+    })
+
+
+def authorization_failure_response():
+    return JsonResponse(status=403, data={
+        'reason': (
+            "The request's authentication details do not corresond "
+            "to the required access level for accessing this resource")
+    })
