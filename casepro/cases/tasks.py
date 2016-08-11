d5948a2569ce48c0845cd04a12b68bf8520d0750
Jason Paidoussi <jason@paidoussi.net>
2016-08-11 08:54:48 +0200

    Added case re-assignment tasks

--- casepro/cases/tasks.py
+++ casepro/cases/tasks.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
-from celery import shared_task
+from celery import shared_task, group
 from celery.utils.log import get_task_logger
+from django.utils.translation import ugettext_lazy as _
 
 logger = get_task_logger(__name__)
 
@@ -13,3 +14,29 @@ def case_export(export_id):
     logger.info("Starting case export #%d..." % export_id)
 
     CaseExport.objects.get(pk=export_id).do_export()
+
+
+@shared_task(ignore_result=True)
+def get_all_cases_passed_response_time():
+    from .models import Case
+    cases = Case.get_all_passed_response_time()
+    tasks = []
+    for case in cases:
+        tasks.append(reassign_case.s(case.pk))
+
+    if tasks:
+        group_task = group(tasks)
+        group_task.apply_async()
+
+
+@shared_task(ignore_result=True)
+def reassign_case(case_id):
+    from .models import Case
+    try:
+        case = Case.objects.get(pk=case_id)
+    except Case.DoesNotExist:
+        logger.warn("Could not load case #%d for re-assignment" % case_id)
+        return
+
+    note = _("This case's required response time as passed and therefor has been re-assigned")
+    case.reassign(None, case.last_assignee, note=note, user_assignee=case.last_user_assignee)
