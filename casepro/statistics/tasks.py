17c6cdf5a32bd0de0257067f320f67811b86f9a7
Jason Paidoussi <jason@paidoussi.net>
2016-08-18 10:11:39 +0200

    Added MinuteTotalCount squash to periodic squash task

--- casepro/statistics/tasks.py
+++ casepro/statistics/tasks.py
@@ -11,10 +11,11 @@ def squash_counts():
     """
     Task to squash all daily counts
     """
-    from .models import TotalCount, DailyCount
+    from .models import TotalCount, DailyCount, MinuteTotalCount
 
     TotalCount.squash()
     DailyCount.squash()
+    MinuteTotalCount.squash()
 
 
 @shared_task
