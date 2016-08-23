8858ecf6c11c96e1e4f51ffc3e571ae4f179e86e
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-08-23 12:28:01 +0200

    Extend min_lenth for Contact.urns base_type and fixed help_text

---
+++ casepro/contacts/migrations/0024_auto_20160823_1024.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.contrib.postgres.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contacts', '0023_contact_urns'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='contact',
+            name='urns',
+            field=django.contrib.postgres.fields.ArrayField(default=list, help_text="List of URNs of the format 'scheme:path'", base_field=models.CharField(max_length=255), size=None),
+        ),
+    ]
