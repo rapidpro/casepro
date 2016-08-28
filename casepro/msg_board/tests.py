10b03bcb2b08174fb540230e683c583b126edc09
Codie Roelf <codie@praekelt.com>
2016-08-29 00:38:19 +0200

    adding test for pin and unpin comment

--- casepro/msg_board/tests.py
+++ casepro/msg_board/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 from django.test import Client
 from django_comments.models import Comment
 from django_comments.forms import CommentForm
+from casepro.msg_board.models import PinnedComment
 
 
 class CommentTest(BaseCasesTest):
@@ -15,7 +16,7 @@ class CommentTest(BaseCasesTest):
         self.login(self.user1)
 
     def test_post_comment(self):
-        data = CommentForm(self.user1, {}).generate_security_data()
+        data = CommentForm(self.unicef).generate_security_data()
         data.update({
             'name': 'the supplied name',
             'comment': 'Foo',
@@ -27,26 +28,44 @@ class CommentTest(BaseCasesTest):
         self.assertEqual(Comment.objects.all().count(), 1)
         self.assertEqual(Comment.objects.all().first().comment, 'Foo')
 
-    def test_view_comments(self):
-        data = CommentForm(self.user1, {}).generate_security_data()
+    def test_pin_comment(self):
+        self.assertEqual(PinnedComment.objects.all().count(), 0)
+        data = CommentForm(target_object=self.unicef).generate_security_data()
         data.update({
             'name': 'the supplied name',
-            'comment': 'Foo',
+            'comment': 'Foo'
         })
-        self.url_post(
-            'unicef',
+        response = self.url_post(
+            self.unicef,
             reverse('comments-post-comment'), data)
-        data = CommentForm(self.user1, {}).generate_security_data()
-        data.update({
-            'name': 'the supplied name',
-            'comment': 'Foo2',
-        })
-        self.url_post(
-            'unicef',
-            reverse('comments-post-comment'), data)
-        url = reverse('msg_board.comment_list')
-        self.login(self.admin)
-        response = self.url_get('unicef', url)
+        self.assertEqual(Comment.objects.all().count(), 1)
+        response = self.url_post(
+            self.unicef,
+            reverse('msg_board.pinnedcomment_pin', kwargs={'pk': Comment.objects.all().first().pk})
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(PinnedComment.objects.all().count(), 1)
 
-        print response
-        self.assertContains(response, 'Foo2')
+    def test_unpin_comment(self):
+        self.assertEqual(PinnedComment.objects.all().count(), 0)
+        data = CommentForm(target_object=self.unicef).generate_security_data()
+        data.update({
+            'name': 'the supplied name',
+            'comment': 'Foo'
+        })
+        response = self.url_post(
+            self.unicef,
+            reverse('comments-post-comment'), data)
+        self.assertEqual(Comment.objects.all().count(), 1)
+        response = self.url_post(
+            self.unicef,
+            reverse('msg_board.pinnedcomment_pin', kwargs={'pk': Comment.objects.all().first().pk})
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(PinnedComment.objects.all().count(), 1)
+        response = self.url_post(
+            self.unicef,
+            reverse('msg_board.pinnedcomment_unpin', kwargs={'pk': Comment.objects.all().first().pk})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(PinnedComment.objects.all().count(), 0)
