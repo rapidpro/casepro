78b8434ee035406e4e904efc66ca265ac033cb65
Jason Paidoussi <jason@paidoussi.net>
2016-08-31 10:04:20 +0200

    Removed unused import

--- casepro/msgs/views.py
+++ casepro/msgs/views.py
@@ -19,7 +19,7 @@ from temba_client.utils import parse_iso8601
 
 from casepro.rules.mixins import RuleFormMixin
 from casepro.statistics.models import DailyCount
-from casepro.utils import parse_csv, str_to_bool, JSONEncoder, json_encode, month_range, get_language_name
+from casepro.utils import parse_csv, str_to_bool, JSONEncoder, json_encode, month_range
 from casepro.utils.export import BaseDownloadView
 
 
