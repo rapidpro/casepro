0796819da1b616e1199ec30ecc39b950d6e7a8ad
Milton Madanda <madandat@gmail.com>
2016-09-01 09:12:57 +0200

    change to use custom comment model

--- casepro/msg_board/urls.py
+++ casepro/msg_board/urls.py
@@ -2,11 +2,10 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from .views import MessageBoardView, CommentsView, PinnedCommentsCRUDL
+from .views import MessageBoardView, CommentsCRUDL
 
-urlpatterns = PinnedCommentsCRUDL().as_urlpatterns()
+urlpatterns = CommentsCRUDL().as_urlpatterns()
 
 urlpatterns += [
     url(r'^messageboard/$', MessageBoardView.as_view(), name='msg_board.comment_list'),
-    url(r'^messageboard/comments/$', CommentsView.as_view(), name='msg_board.comments'),
 ]
--- casepro/msg_board/views.py
+++ casepro/msg_board/views.py
@@ -1,16 +1,14 @@
 from __future__ import unicode_literals
 
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
+from django.utils import timezone
 
 from smartmin.views import SmartListView, SmartTemplateView
 from smartmin.views import SmartReadView, SmartCRUDL
-from django_comments.models import Comment
 from dash.orgs.models import Org
 from dash.orgs.views import OrgPermsMixin, OrgObjPermsMixin
-from casepro.utils import JSONEncoder
-from casepro.msg_board.models import PinnedComment
+from casepro.msg_board.models import MessageBoardComment
 
 
 class MessageBoardView(OrgPermsMixin, SmartTemplateView):
@@ -18,62 +16,44 @@ class MessageBoardView(OrgPermsMixin, SmartTemplateView):
     permission = 'orgs.org_inbox'
 
 
-class CommentsView(OrgPermsMixin, SmartListView):
-    """
-    JSON endpoint for listing comments
-    """
-    permission = 'orgs.org_inbox'
-    title = 'Message Board'
-
-    def get_queryset(self):
-        return Comment.objects.for_model(Org).filter(object_pk=self.request.org.pk).order_by('-submit_date')
-
-    def render_to_response(self, context, **response_kwargs):
-        comments = [{
-            'comment': c.comment,
-            'comment_id': c.id,
-            'user_name': c.user_name,
-            'user_id': c.user_id,
-            'submit_date': c.submit_date,
-        } for c in self.get_queryset()]
-        return JsonResponse({'results': comments}, encoder=JSONEncoder)
-
-
-class PinnedCommentsCRUDL(SmartCRUDL):
-    title = 'Pinned Comments'
-    actions = ('list', 'pin', 'unpin')
-    model = PinnedComment
+class CommentsCRUDL(SmartCRUDL):
+    title = 'Comments'
+    actions = ('list', 'pinned', 'pin', 'unpin')
+    model = MessageBoardComment
 
     class List(OrgPermsMixin, SmartListView):
+        permission = 'msg_board.messageboardcomment_list'
+        default_order = ('-submit_date',)
+
+        def get_queryset(self):
+            return MessageBoardComment.objects.for_model(Org).filter(
+                object_pk=self.request.org.pk).order_by('-submit_date')
+
+        def get(self, request, *args, **kwargs):
+            return JsonResponse({'results': [c.as_json() for c in self.get_queryset()]})
+
+    class Pinned(OrgPermsMixin, SmartListView):
+        permission = 'msg_board.messageboardcomment_pinned'
         default_order = ('-pinned_date',)
 
         def get_queryset(self):
-            return super(PinnedCommentsCRUDL.List, self).get_queryset().filter(org=self.request.org)
+            return MessageBoardComment.objects.for_model(Org).filter(
+                object_pk=self.request.org.pk,
+                is_pinned=True).order_by('-pinned_date')
 
         def get(self, request, *args, **kwargs):
-            def as_json(pinned_comment):
-                return {
-                    'comment': pinned_comment.comment.comment,
-                    'user_name': pinned_comment.comment.user_name,
-                    'user_id': pinned_comment.comment.user_id,
-                    'submit_date': pinned_comment.comment.submit_date,
-                    'pinned_date': pinned_comment.pinned_date,
-                    'owner_id': pinned_comment.owner.id,
-                    'comment_id': pinned_comment.comment.id,
-                }
-
-            return JsonResponse({'results': [as_json(c) for c in self.get_queryset()]})
+            return JsonResponse({'results': [c.as_json() for c in self.get_queryset()]})
 
     class Pin(OrgObjPermsMixin, SmartReadView):
         """
         Endpoint for creating a Pinned Comment
         """
-        permission = 'msg_board.pinnedcomment_pin'
+        permission = 'msg_board.messageboardcomment_pin'
         fields = ['comment', 'pinned_date']
         http_method_names = ['post']
 
         def get_object(self):
-            comment = Comment.objects.for_model(Org).filter(
+            comment = MessageBoardComment.objects.for_model(Org).filter(
                 object_pk=self.request.org.pk,
                 pk=self.kwargs.get('pk')).first()
 
@@ -84,21 +64,24 @@ class PinnedCommentsCRUDL(SmartCRUDL):
             return comment
 
         def post(self, request, *args, **kwargs):
-            pinned_comment, created = PinnedComment.objects.get_or_create(
-                comment=self.get_object(),
-                defaults={'owner': request.user, 'org': request.org})
-            return HttpResponse(status=201 if created else 200)
+            comment = self.get_object()
+            if not comment.is_pinned:
+                comment.is_pinned = True
+                comment.pinned_date = timezone.now()
+                comment.save()
+                return HttpResponse(status=204)
+            return HttpResponse(status=200)
 
     class Unpin(OrgObjPermsMixin, SmartReadView):
         """
         Endpoint for deleting a Pinned Comment
         """
-        permission = 'msg_board.pinnedcomment_pin'
+        permission = 'msg_board.messageboardcomment_unpin'
         fields = ['comment', 'pinned_date']
         http_method_names = ['post']
 
         def get_object(self):
-            comment = Comment.objects.for_model(Org).filter(
+            comment = MessageBoardComment.objects.for_model(Org).filter(
                 object_pk=self.request.org.pk,
                 pk=self.kwargs.get('pk')).first()
 
@@ -109,6 +92,9 @@ class PinnedCommentsCRUDL(SmartCRUDL):
             return comment
 
         def post(self, request, *args, **kwargs):
-            pinned_comment = get_object_or_404(PinnedComment, comment=self.get_object())
-            pinned_comment.delete()
+            comment = self.get_object()
+            if comment.is_pinned:
+                comment.is_pinned = False
+                comment.save()
+                return HttpResponse(status=204)
             return HttpResponse(status=200)
