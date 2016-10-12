ab44f2f7bee1d3266f9ae0ff9ba000095766056d
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-10-12 17:24:20 +0200

    Add setting to specify contact name display format

--- casepro/settings_common.py
+++ casepro/settings_common.py
@@ -57,6 +57,7 @@ SITE_ORGS_STORAGE_ROOT = 'orgs'
 SITE_EXTERNAL_CONTACT_URL = 'http://localhost:8001/contact/read/%s/'
 SITE_BACKEND = 'casepro.backend.NoopBackend'
 SITE_ANON_CONTACTS = False
+SITE_CONTACT_DISPLAY = "urn"
 
 # junebug configuration
 JUNEBUG_API_ROOT = 'http://localhost:8080/'
