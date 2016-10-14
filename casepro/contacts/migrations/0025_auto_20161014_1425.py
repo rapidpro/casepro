194d80e18897c962404ef54a8674145062bb9c47
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-10-14 16:29:21 +0200

    Fixed contacts migrations

--- casepro/contacts/migrations/0025_auto_20161014_1425.py
+++ casepro/contacts/migrations/0025_auto_20161014_1425.py
@@ -8,7 +8,7 @@ import django.contrib.postgres.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0023_contact_urns'),
+        ('contacts', '0024_auto_20160822_1224'),
     ]
 
     operations = [
