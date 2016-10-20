493ab5e50d27c1834407163d5e7014ae90b211bb
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-10-20 10:48:14 +0200

    Combined contacts migrations

--- casepro/contacts/migrations/0023_auto_20161020_0823.py
+++ casepro/contacts/migrations/0023_auto_20161020_0823.py
@@ -8,13 +8,18 @@ import django.contrib.postgres.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0024_auto_20160822_1224'),
+        ('contacts', '0022_contact_is_stopped_pt2'),
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='contact',
             name='urns',
             field=django.contrib.postgres.fields.ArrayField(default=list, help_text="List of URNs of the format 'scheme:path'", base_field=models.CharField(max_length=255), size=None),
+        ),
+        migrations.AlterField(
+            model_name='contact',
+            name='uuid',
+            field=models.CharField(max_length=36, unique=True, null=True),
         ),
     ]
