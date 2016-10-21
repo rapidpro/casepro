6ddf2179fa6eddda8d247b7c3a4310102c6976d3
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-10-21 10:16:43 +0200

    Show 'URN' instead of 'Name' for the contact on a case if display format is urns

--- casepro/cases/views.py
+++ casepro/cases/views.py
@@ -74,6 +74,7 @@ class CaseCRUDL(SmartCRUDL):
             })
 
             context['anon_contacts'] = getattr(settings, 'SITE_ANON_CONTACTS', False)
+            context['contact_display_format'] = getattr(settings, 'SITE_CONTACT_DISPLAY', "")
             context['max_msg_chars'] = MAX_MESSAGE_CHARS
             context['can_update'] = can_update
             context['alert'] = self.request.GET.get('alert', None)
