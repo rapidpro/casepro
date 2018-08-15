df1babf3e5943213ced3ad5baee980d880af49d7
Rowan Seymour <rowanseymour@gmail.com>
2018-08-15 17:03:51 -0500

    Complete cases read endpoint

--- casepro/api/serializers.py
+++ casepro/api/serializers.py
@@ -1,8 +1,25 @@
+import pytz
+
 from rest_framework import serializers
 
 from casepro.cases.models import Case
 
 class CaseSerializer(serializers.ModelSerializer):
+    labels = serializers.SerializerMethodField()
+    assignee = serializers.SerializerMethodField()
+    contact = serializers.SerializerMethodField()
+    opened_on = serializers.DateTimeField(default_timezone=pytz.UTC)
+    closed_on = serializers.DateTimeField(default_timezone=pytz.UTC)
+
+    def get_labels(self, obj):
+        return [{"uuid": str(l.uuid), "name": l.name} for l in obj.labels.all()]
+
+    def get_assignee(self, obj):
+        return {"id": obj.assignee.id, "name": obj.assignee.name}
+
+    def get_contact(self, obj):
+        return {"uuid": str(obj.contact.uuid)}
+
     class Meta:
         model = Case
-        fields = ('summary', 'opened_on', 'closed_on')
+        fields = ('id', 'summary', 'labels', 'assignee', 'contact', 'opened_on', 'closed_on')
--- casepro/api/tests.py
+++ casepro/api/tests.py
@@ -1,9 +1,19 @@
+import pytz
+
 from django.urls import reverse
 
 from casepro.test import BaseCasesTest
 
 
 class APITest(BaseCasesTest):
+
+    def setUp(self):
+        super(APITest, self).setUp()
+
+        self.ann = self.create_contact(
+            self.unicef, "C-001", "Ann", fields={"age": "34"}, groups=[self.females, self.reporters]
+        )
+        self.bob = self.create_contact(self.unicef, "C-002", "Bob")
 
     def test_authentication(self):
         url = reverse("api.case-list")
@@ -17,7 +27,6 @@ class APITest(BaseCasesTest):
 
         response = self.url_get("unicef", url, HTTP_AUTHORIZATION="Token %s" % self.admin.auth_token.key)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'count': 0, 'next': None, 'previous': None, 'results': []})
 
         # test with session authentication (with non-admin user)
         self.login(self.user1)
@@ -30,4 +39,51 @@ class APITest(BaseCasesTest):
 
         response = self.url_get("unicef", url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'count': 0, 'next': None, 'previous': None, 'results': []})
+
+    def test_cases(self):
+        msg1 = self.create_message(self.unicef, 101, self.ann, "Hello", [self.aids])
+        msg2 = self.create_message(self.unicef, 102, self.bob, "Bonjour")
+        case1 = self.create_case(self.unicef, self.ann, self.moh, msg1, [self.aids], summary="Summary 1",
+                                 user_assignee=self.user1)
+        case2 = self.create_case(self.unicef, self.ann, self.who, msg2, [], summary="Summary 2",
+                                 user_assignee=self.user1)
+
+        # case for another org
+        msg3 = self.create_message(self.nyaruka, 103, self.bob, "Hola")
+        self.create_case(self.nyaruka, self.ann, self.klab, msg3, [self.code], summary="Summary 3",
+                                 user_assignee=self.user4)
+
+        url = reverse("api.case-list")
+        self.login(self.admin)
+
+        response = self.url_get("unicef", url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['results'], [
+            {
+                'id': case2.id,
+                'labels': [],
+                'assignee': {'id': self.who.id, 'name': 'WHO'},
+                'contact': {"uuid": str(case2.contact.uuid)},
+                'opened_on': self._format_date(case2.opened_on),
+                'closed_on': None,
+                'summary': 'Summary 2',
+            },
+            {
+                'id': case1.id,
+                'labels': [{"uuid": "L-001", "name": "AIDS"}],
+                'assignee': {'id': self.moh.id, 'name': 'MOH'},
+                'contact': {"uuid": str(case1.contact.uuid)},
+                'opened_on': self._format_date(case1.opened_on),
+                'closed_on': None,
+                'summary': 'Summary 1',
+            }
+        ])
+
+    def _format_date(self, d):
+        """
+        Helper function which formats a date exactly how DRF's DateTimeField does it
+        """
+        s = d.isoformat()
+        if s.endswith('+00:00'):
+            s = s[:-6] + 'Z'
+        return s
