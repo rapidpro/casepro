f09b807a61ae4480eed37a61bba392a969b3f4a1
Matheus <matheus.msa27@gmail.com>
2019-12-12 14:33:30 -0300

    remove comments from docker files

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -13,6 +13,3 @@ python /app/manage.py compilemessages
 
 echo "Starting server"
 /usr/local/bin/gunicorn casepro.wsgi:application -t 120 -w 2 --max-requests 5000 -b 0.0.0.0:8000
-
-#echo "Starting server"
-#python manage.py runserver 127.0.0.0:8000
