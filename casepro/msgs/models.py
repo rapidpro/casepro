96e78b2d59e5d77ba041416916af8508964386a9
Rowan Seymour <rowanseymour@gmail.com>
2020-01-14 14:14:46 -0500

    Fix searching by text and date

--- casepro/msgs/models.py
+++ casepro/msgs/models.py
@@ -458,12 +458,12 @@ class Message(models.Model):
 
         # handle views that don't require filtering by any labels
         if folder == MessageFolder.unlabelled:
-            return msgs.filter(type=Message.TYPE_INBOX, has_labels=False).filter(**msg_filtering, **lbl_filtering)
+            return msgs.filter(type=Message.TYPE_INBOX, has_labels=False).filter(**msg_filtering).filter(**lbl_filtering)
         if all_label_access and not label_id:
             if folder == MessageFolder.inbox:
-                return msgs.filter(has_labels=True).filter(**msg_filtering, **lbl_filtering)
+                return msgs.filter(has_labels=True).filter(**msg_filtering).filter(**lbl_filtering)
             else:
-                return msgs.filter(**msg_filtering, **lbl_filtering)
+                return msgs.filter(**msg_filtering).filter(**lbl_filtering)
 
         # we either want messages with a specific label or any of the labels this user has access to
         labels = Label.get_all(org, user)
