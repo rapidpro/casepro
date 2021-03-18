72bfedab76f0182d9d0132870eddefc5ba4ee8d2
Rowan Seymour <rowanseymour@gmail.com>
2021-03-18 16:10:08 -0500

    Add support for partial syncing of contacts

--- casepro/contacts/tasks.py
+++ casepro/contacts/tasks.py
@@ -1,3 +1,4 @@
+import iso8601
 from dash.orgs.tasks import org_task
 
 from celery.utils.log import get_task_logger
@@ -5,7 +6,7 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 
-@org_task("contact-pull", lock_timeout=12 * 60 * 60)
+@org_task("contact-pull", lock_timeout=2 * 60 * 60)
 def pull_contacts(org, since, until, prev_results):
     """
     Fetches updated contacts from RapidPro and updates local contacts accordingly
@@ -22,10 +23,34 @@ def pull_contacts(org, since, until, prev_results):
     def progress(num):  # pragma: no cover
         logger.debug(f" > Synced {num} contacts for org #{org.id}")
 
-    contacts_created, contacts_updated, contacts_deleted, _, _ = backend.pull_contacts(org, since, until, progress)
+    cursor = None
+
+    if prev_results:
+        prev_resume = prev_results["contacts"].get("resume")
+        prev_until = prev_results["contacts"].get("until")
+
+        if prev_resume:
+            cursor = prev_resume["cursor"]
+            since = iso8601.parse_date(prev_resume["since"])
+            until = iso8601.parse_date(prev_resume["until"])
+
+            logger.warn(f"Resuming previous incomplete sync for #{org.id}")
+        elif prev_until:
+            since = iso8601.parse_date(prev_until)
+
+    contacts_created, contacts_updated, contacts_deleted, _, new_cursor = backend.pull_contacts(
+        org, since, until, progress, resume_cursor=cursor
+    )
+
+    contacts_results = {"created": contacts_created, "updated": contacts_updated, "deleted": contacts_deleted}
+
+    if new_cursor:
+        contacts_results["resume"] = {"cursor": new_cursor, "since": since.isoformat(), "until": until.isoformat()}
+    else:
+        contacts_results["until"] = until.isoformat()
 
     return {
         "fields": {"created": fields_created, "updated": fields_updated, "deleted": fields_deleted},
         "groups": {"created": groups_created, "updated": groups_updated, "deleted": groups_deleted},
-        "contacts": {"created": contacts_created, "updated": contacts_updated, "deleted": contacts_deleted},
+        "contacts": contacts_results,
     }
